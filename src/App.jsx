fa4c79e15c03f8c39bd271ac7ba82df2d1de899b
ArnauNeches <arnichi2103@gmail.com>
2026-01-23 23:57:30 +0100

    installed tailwind and uuid

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <div className="bg-amber-400 h-screen flex flex-wrap justify-around items-center">
+        <div className="w-10 h-10 bg-amber-700"></div>
+        <div className="w-10 h-10 bg-amber-700"></div>
+      </div>
     </>
   )
 }
