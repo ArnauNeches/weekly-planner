f65507f1ee90ac2bd8f85bb5a968e538748fe704
ArnauNeches <arnichi2103@gmail.com>
2026-01-26 00:49:00 +0100

    custom hook (cleanup crud logic and state)

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,23 @@
-import { useEffect, useState } from "react";
 import DayColumn from "./components/dayColumn"
-import {initialData, DAYS} from "./data/initialData"
-import {v4 as uuidv4} from 'uuid';
+import { DAYS } from "./data/initialData"
+import { useWeeklyPlanner } from "./hooks/useWeeklyPlanner";
 
 function App() {
-  const [weekData, setWeekData] = useState( () => {
-    const storage = localStorage.getItem("weekly-planner-data");
-    if (storage) return JSON.parse(storage);
-    return initialData;
-  }
-);
-  
-  useEffect(() => {
-    const newData = JSON.stringify(weekData);
-    localStorage.setItem("weekly-planner-data", newData);
-  }, [weekData]);
-
-  function handleDeleteTask(day, id){
-    const dayKey = day.toLowerCase();
-
-    setWeekData(prevWeekData => {
-      return ({
-        ...prevWeekData,
-        [dayKey]: [
-          ...prevWeekData[dayKey].filter(task => task.id !== id)
-        ]
-      })
-    });
-  }
-
-  function handleChangeStatus(day, newStatus, id){
-    const dayKey = day.toLowerCase();
-
-    setWeekData(prev => {
-      const dayTasks = prev[dayKey];
-      const updatedTasks = dayTasks.map(task => {
-        if (task.id == id) return {...task, status: newStatus};
-        return task;
-      });
-
-      return {
-        ...prev,
-        [dayKey]: updatedTasks
-      };
-  });
-}
-
-  function handleAddTask(day, text){
-
-    setWeekData(prevWeekData => {
-      const newEntry = {
-        id: uuidv4(),
-        name: text,
-        status: 'pending',
-        createdAt: new Date().toLocaleDateString()
-      }
-
-      return ({
-          ...prevWeekData,
-          [day.toLowerCase()]: [
-            ...prevWeekData[day.toLowerCase()],
-            newEntry
-          ]
-        })
-      });
-  }
+  const { weekData, addTask, deleteTask, changeStatus } = useWeeklyPlanner()
 
   return (
     <>
       <h1 className="text-4xl text-white font-bold text-center mb-8">Weekly Planner</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
-        {DAYS.map(day => <DayColumn key={day} tasks={weekData[day]} onNewTask={handleAddTask} onChangeStatus={handleChangeStatus} onDeleteTask={handleDeleteTask}>{day.toUpperCase()}</DayColumn>)}
+        {DAYS.map(day => <DayColumn
+          key={day} 
+          tasks={weekData[day]} 
+          onNewTask={addTask} 
+          onChangeStatus={changeStatus} 
+          onDeleteTask={deleteTask}
+          >
+            {day.toUpperCase()}
+          </DayColumn>)}
       </div>
     </>
   );
---
+++ src/hooks/useWeeklyPlanner.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+import { initialData } from "../data/initialData";
+
+export function useWeeklyPlanner() {
+
+    const [weekData, setWeekData] = useState(() => {
+        const storage = localStorage.getItem("weekly-planner-data");
+        if (storage) return JSON.parse(storage);
+        return initialData;
+    }
+    );
+
+    useEffect(() => {
+        const newData = JSON.stringify(weekData);
+        localStorage.setItem("weekly-planner-data", newData);
+    }, [weekData]);
+
+    function deleteTask(day, id) {
+        const dayKey = day.toLowerCase();
+
+        setWeekData(prevWeekData => {
+            return ({
+                ...prevWeekData,
+                [dayKey]: [
+                    ...prevWeekData[dayKey].filter(task => task.id !== id)
+                ]
+            })
+        });
+    }
+
+    function changeStatus(day, newStatus, id) {
+        const dayKey = day.toLowerCase();
+
+        setWeekData(prev => {
+            const dayTasks = prev[dayKey];
+            const updatedTasks = dayTasks.map(task => {
+                if (task.id == id) return { ...task, status: newStatus };
+                return task;
+            });
+
+            return {
+                ...prev,
+                [dayKey]: updatedTasks
+            };
+        });
+    }
+
+    function addTask(day, text) {
+
+        setWeekData(prevWeekData => {
+            const newEntry = {
+                id: uuidv4(),
+                name: text,
+                status: 'pending',
+                createdAt: new Date().toLocaleDateString()
+            }
+
+            return ({
+                ...prevWeekData,
+                [day.toLowerCase()]: [
+                    ...prevWeekData[day.toLowerCase()],
+                    newEntry
+                ]
+            })
+        });
+    }
+
+    return {weekData, addTask, changeStatus, deleteTask}
+}
