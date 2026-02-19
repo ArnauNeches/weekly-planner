44476fb23c87ea9a8f6e7478569ab2ee24eb8558
ArnauNeches <arnichi2103@gmail.com>
2026-02-19 01:41:21 +0100

    task model and db migration

--- backend/database.py
+++ backend/database.py
@@ -16,7 +16,8 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = DeclarativeBase()
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from database import engine, Base
+from models import task
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "hello world"}
+    return {"Hello": "World"}
