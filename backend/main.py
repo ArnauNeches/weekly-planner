ac2938997574db13872cef3212926774267bdc9f
ArnauNeches <arnichi2103@gmail.com>
2026-02-19 06:31:38 +0100

    serve frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from database import engine, Base
 from routers import tasks
 
@@ -8,6 +11,11 @@ app = FastAPI()
 
 app.include_router(tasks.router)
 
+# Serve frontend
+
+frontend_dist_path = Path("../frontend/dist")
+app.mount("/assets", StaticFiles(directory=frontend_dist_path / "assets"), name="static")
+
 @app.get("/")
-def health_check():
-    return {"status": "online"}
+async def serve_frontend():
+    return FileResponse(frontend_dist_path / "index.html")
